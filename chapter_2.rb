7d515134ba9b5c7f9bae5bd840af5274aede35c9
abhishekgarfield <abhishek2759@gmail.com>
2023-01-14 08:49:59 +0530

    chapter 2

---
+++ chapter_2.rb
@@ -0,0 +1,13 @@
+# classes and objects
+
+class First_class   # classname starts with a capital latter 
+    def say_something
+        puts "hello"
+    end
+end
+
+
+first_obj = First_class.new()
+
+first_obj.say_something()
+
