446c6ab646373c329a127c107479ccbbb8075e48
abhishekgarfield <abhishek2759@gmail.com>
2023-01-15 22:39:36 +0530

    CHANGE

--- chapter_2.rb
+++ chapter_2.rb
@@ -10,4 +10,16 @@ end
 first_obj = First_class.new()
 
 first_obj.say_something()
+puts "#{first_obj.class()}" # for printing classname
 
+
+
+# instance variables 
+An instance variable in ruby has a name starting with @ symbol
+instance variables have value nil before initialization 
+The instance variables of an object can only be accessed by the instance methods of that object.
+An instance variable belongs to the object itself (each object has its own instance variable of that particular class)
+
+
+
+  
