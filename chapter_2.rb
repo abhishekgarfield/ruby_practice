8027d1a15b2109b39ad48f850dbef3bdcd5729b1
abhishekgarfield <abhishek2759@gmail.com>
2023-01-30 22:30:11 +0530

    commit

--- chapter_2.rb
+++ chapter_2.rb
@@ -6,7 +6,7 @@ class First_class   # classname starts with a capital latter
     end
 end
 
-
+  
 first_obj = First_class.new()
 
 first_obj.say_something()
@@ -23,7 +23,7 @@ An instance variable belongs to the object itself (each object has its own insta
     
        
 
-#"9991".match('^(([0-9]{1,3})([:]?)([0-5][0-9]))$')
+# "9991".match('^(([0-9]{1,3})([:]?)([0-5][0-9]))$')
 =end
     
   
